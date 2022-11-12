99dad8c7cb0b33554d34936e8f5dfe0045052ee1
UrmiTari <urmitari@gmail.com>
2022-11-12 16:43:43 +0200

    footer

--- src/components/FooterCompo.vue
+++ src/components/FooterCompo.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <p>© 1996 Anette Habanen, Urmi Tari, Priidik Västrik</p>
+        <p>© 2022 Anette Habanen, Urmi Tari, Priidik Västrik</p>
     </footer>
 
 </template>
