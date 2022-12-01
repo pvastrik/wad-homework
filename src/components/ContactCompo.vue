be2d05b03b51147428e38c84be7a4628d4b0791a
anettehabanen <anette.habanen@gmail.com>
2022-12-01 17:19:40 +0200

    Contacts page info added

--- src/components/ContactCompo.vue
+++ src/components/ContactCompo.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="aboutus">
+    <div class="item">
+        <h2 class="textformating">Team: C</h2>
+        <h2 class="textformating">Homework: 4</h2>
+        <h2 class="textformating">Location: The Delta Centre</h2>
+        <h2 class="textformating">Opening hours: from midnight to midnight</h2>
+        <br>
+        <h2 class="textformating">If you already are an user, log in. If you do not have an account, please sign up.</h2>
+    </div>
     <router-link to="/signup" custom v-slot="{navigate}"><button @click="navigate" role="link">Sign up</button></router-link>
     <router-link to="/login" custom v-slot="{navigate}"><button @click="navigate" role="link">Log in</button></router-link>
-
+    
   </div>
 </template>
 
@@ -23,23 +31,24 @@ button {
   text-align: center;
   align-items: center;
   background-color: #ffd2fc;
-  border-radius: 5%;
+  border-radius: 20px;
   font-size: 3em;
   border-color: #ffd2fc;
 }
 
-h3 {
-  margin: 40px 0 0;
+.textformating {
+  font-size: 1em;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.item {
+  order: inherit;
+  background-color: #ffd2fc;
+  margin: 4em;
+  align-items: center;
+  justify-content: center;
+  padding: 1em;
+  border-radius: 20px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
